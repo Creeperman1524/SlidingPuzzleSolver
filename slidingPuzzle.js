a971cacab53038fe61d9e85fe3cad293f67a5403
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2023-10-18 12:34:09 -0400

    Displays the puzzle
    Displays the puzzle into the console with a nicer format

--- slidingPuzzle.js
+++ slidingPuzzle.js
@@ -1,5 +1,11 @@
 // The completed puzzle
 // The 0 tile represents the space
+/**
+0  1  2  3
+4  5  6  7
+8  9  10 11
+12 13 14 15
+*/
 COMPLETED_PUZZLE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]
 
 // Generates a random sliding puzzle (not guaranteed to be solvable ?)
@@ -18,10 +24,34 @@ function generatePuzzle() {
 	return puzzle
 }
 
+function displayPuzzle(puzzle) {
+	let display = ''
+
+	const horizontalBar = '\n---------------------\n'
+	for (let i = 0; i < puzzle.length; i++) {
+		if(i % 4 == 0) display += horizontalBar
+		display += `| ${puzzle[i]} ` + (puzzle[i] >= 10 ? '' : ' ')
+		if(i % 4 == 3) display += '|'
+	}
+	
+	display += horizontalBar
+
+	console.log(display)
+}
+
+// A solver that uses the BDF algorithm
+function solvePuzzleBDF(puzzle) {
+	const queue = []
+
+	states = nextStates(puzzle)
+
+
+}
+
 function main() {
 	puzzle = generatePuzzle()
 
-	console.log(puzzle)
+	displayPuzzle(puzzle)
 }
 
 main()
